702af5b686b15f3e958d6a739f5754574d289daf
Muniratize <muniratyakubu62@gmail.com>
2022-03-10 15:35:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
  
 /**
-* main - entry point
+* main - print the string in the put function
 *
-* Return: Always 0 (Success)
+* Description: using the main function
+* This program should print "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
-{puts("\"Programming is like building a mulitilingual puzzle\n");       
-return (0);
+{
+puts("\"Programming is like building a mulitilingual puzzle\n") return (0);
 }
