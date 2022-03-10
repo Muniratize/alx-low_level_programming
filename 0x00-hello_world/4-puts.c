eef996ceab882a716d0bf0aa99321d231e53b2d3
Muniratize <muniratyakubu62@gmail.com>
2022-03-10 15:56:19 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 /*
 * main - entry point
 *
-* Description: using the main function
+* Description: using the main functio
+* this program prints "Programming is like building a multilingual puzzle
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
