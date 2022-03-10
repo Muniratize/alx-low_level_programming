f87421770ded079935f2739ca4eb2501348f90fc
Muniratize <muniratyakubu62@gmail.com>
2022-03-10 15:28:10 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
  
-/*
+/**
 * main - entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
-{	
-	puts("\"Programming is like building a mulitilingual puzzle\n");       
+{puts("\"Programming is like building a mulitilingual puzzle\n");       
 return (0);
 }
