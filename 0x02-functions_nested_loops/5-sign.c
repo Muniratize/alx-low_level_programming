9eef90a4898b8b065b1e416ff12a93782199196d
Muniratize <muniratyakubu62@gmail.com>
2022-03-16 12:01:38 -0700

    printing sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_sign - a function that prints the sign of a number
- *@n: the number to print the sign
- * Return: the sign of n
+ *print_sign - prints the sign of a number
+ *@n: the nunber to print the sign
+ *Return: the sign of n
  */
 int print_sign(int n)
 {
-	if (n > 0 )
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == '0')
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
