23f7385fecf8bd0d16d39a3deb71f05ff0280e62
Muniratize <muniratyakubu62@gmail.com>
2022-03-16 11:39:59 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isalpha - function that checks for alphabetic character
- *@c: character to be checked
- * Return: 1 if c is a letter, lower case or uppercase, returns 0 otherwise
- */
+ * _isalpha  - A function that checks for alphabet.
+ *@c: the character to be checked
+ * Return: 1 if c is alpha, 0 otherwise
+*/
 int _isalpha(int c)
 {
-	if (c <= 'a' && c >= 'z' || c <= 'A' && c >= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
