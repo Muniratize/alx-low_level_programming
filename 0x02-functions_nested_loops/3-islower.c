afc35baf3cd50e7c311acae71dd08d4747a66ee0
Muniratize <muniratyakubu62@gmail.com>
2022-03-15 17:18:44 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lower case character
+ * @c: character to be checked
+ * Return: 1 if c is lower, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c is >= 'a' && c <= 'z');
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
