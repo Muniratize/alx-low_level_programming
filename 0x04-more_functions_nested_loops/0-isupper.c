577c20cf77a28fc8aa2eab526eb10ffaee5a1425
Muniratize <muniratyakubu62@gmail.com>
2022-03-17 16:40:16 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,14 +5,13 @@
  *@c: character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z');
-	{
+	if
+		(c >= 'A' && c <= 'Z');
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
+
